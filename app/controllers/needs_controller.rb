1c5788b5a6a1a393df22a67b39f84392f1a015ab
Jared Roth <JaredRoth@users.noreply.github.com>
2016-05-12 11:31:03 -0600

    Remove set_need as before action
    Relocated it into the appropriate methods because it is not needed in every method

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -3,27 +3,28 @@ class NeedsController < ApplicationController
     @needs = NeedType.all
   end
 
-  # before_action :set_need
-
   def show
+    set_need
   end
 
   def donate
+    set_need
     @need.add_donation(params[:need][:raised])
     redirect_to :back, notice: "Gift of $#{params[:need][:raised]} added to your basket"
   end
 
   def update
+    set_need
   end
 
 
 private
-  #
-  # def set_need
-  #   recipient = User.find_by(username: params[:username])
-  #   byebug
-  #   @need = recipient.needs.find_by(slug: params[:slug])
-  # end
+  
+  def set_need
+    recipient = User.find_by(username: params[:username])
+    byebug
+    @need = recipient.needs.find_by(slug: params[:slug])
+  end
 
   def needs_params
     params.require(:need).permit(:name, :description, :cost, :raised, :category, :image_url )
