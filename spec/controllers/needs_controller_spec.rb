496e0c24a525306147e77bdecc82e43a8e467949
Hedy <hedywoo@gmail.com>
2016-05-12 20:58:52 -0600

    Implement unit test for need and needtype

--- spec/controllers/needs_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe NeedsController, type: :controller do
-
-end
--- spec/models/need_type_spec.rb
+++ spec/models/need_type_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe NeedType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "before_validation" do
+    it { is_expected.to callback(:assign_slug).before(:validation) }
+  end
+
+  context "validation" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:cost) }
+    it { is_expected.to validate_presence_of(:image_url) }
+    #it { is_expected.to validate_presence_of(:max) } #valid for nil, but should have been invalid
+  end
+
+  it "generates a slug when it's created" do
+    need  = create(:need, name:  "Goats And Soda")
+
+    expect(need.slug).to eq("goats-and-soda")
+  end
+
 end
