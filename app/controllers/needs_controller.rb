7861ed414997268e0d80428699e70d4652d1c314
Jared Roth <jared.roth@gmail.com>
2016-05-13 22:01:57 -0600

    Prep to merge from master

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -19,17 +19,26 @@ class NeedsController < ApplicationController
   end
 
   def create
+    rejected = []
+    approved = []
     session[:cart].each do |id, qty|
       need_type = NeedType.find(id)
-
-      current_user.needs.create( name: pluralize(qty, need_type.name),
-                              description: need_type.description,
-                              cost: need_type.cost * qty,
-                              image_url: need_type.image_url,
-                              slug: need_type.slug,
-                              category: need_type.category
-                              )
+      if current_user.needs.exists?(slug: need_type.slug)
+        rejected << need_type.name
+      else
+        current_user.needs.create(name: pluralize(qty, need_type.name),
+                                  description: need_type.description,
+                                  cost: need_type.cost * qty,
+                                  image_url: need_type.image_url,
+                                  slug: need_type.slug,
+                                  category: need_type.category
+                                  )
+        approved << need_type.name
+      end
     end
+    flash[:notice] = "The following needs were added to your profile: #{approved.join(', ')}." unless approved.nil?
+    flash[:error] = "At least one of each of the following needs has already been requested: #{rejected.join(', ')}. Please modify existing requests from your profile." unless rejected.nil?
+    session[:cart] = {}
     redirect_to user_path
   end
 
