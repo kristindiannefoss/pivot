a3d9e43667f991c2b12831ed9a6325b25dc2a5e7
Kris Foss <kris.foss@gmail.com>
2016-05-15 15:23:41 -0600

    users seeded with three different roles, images for all added to assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-#
-# list_of_images = [
-#   {path_to_image: "girl_and_goat.jpg", name: "whatever"},
-#   {path_to_image: "girl_and_goat.jpg", name: "whatever"},
-#   {path_to_image: "girl_and_goat.jpg", name: "whatever"}
-# ]
-# list_of_images = [[ "Lady with Bees": "aws/animals/lady_with_bees.png"],
-#                   [ "name": "/path", "name": "/path"]]
-# Category.create([{ name: "basic needs" },
-#                  { name: "animals" },
-#                  { name: "farming" },
-#                  { name: "health" }])
-
 basic_needs_list = ["auto_maintenance.png",
   "basic_farming_tools.png",
   "bicycle.png",
@@ -151,5 +131,113 @@ health_needs_list.each do |a|
   description: Faker::Lorem.sentence,
   cost: Faker::Commerce.price,
   category_id: Category.find_or_create_by(name: "health").id,
+  image_url: a )
+end
+
+# USERS
+# __________________________________________________________
+recipients_list = [
+  "1.png",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+  "12.jpg",
+  "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+  "17.jpg",
+  "18.jpg",
+  "19.jpg",
+  "20.png",
+  "21.jpg",
+  "22.JPG",
+  "23.jpg",
+  "24.jpg",
+  "25.jpg",
+  "26.jpg",
+  "27.jpg",
+  "28.jpeg",
+  "29.png",
+  "30.jpg",
+  "40.jpg",
+  "41.jpg",
+  "42.jpg",
+  "43.png",
+  "44.jpg",
+  "45.jpg",
+  "46.jpg",
+  "47.jpg",
+  "48.jpg",
+  "49.jpg",
+  "50.jpg",
+  "51.jpg",
+  "ladies.jpg"
+]
+
+donors_list = [
+  "a.jpg",
+  "b.jpg",
+  "c.png",
+  "d.jpeg",
+  "e.jpg",
+  "f.jpg",
+  "g,.jpg",
+  "h.jpg",
+]
+
+admins_list = [
+  "hedy.png",
+  "jared.jpeg",
+  "jon.jpeg",
+  "kris.jpeg",
+]
+
+recipients_list.each do |a|
+  User.create(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.safe_email,
+  city: Faker::Address.city,
+  country: Faker::Address.country,
+  username: Faker::Internet.user_name,
+  password: Faker::Internet.password,
+  role: 2,
+  description: Faker::Lorem.paragraph(3, false, 1),
+  image_url: a )
+end
+
+donors_list.each do |a|
+  User.create(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.safe_email,
+  city: Faker::Address.city,
+  country: Faker::Address.country,
+  username: Faker::Internet.user_name,
+  password: Faker::Internet.password,
+  role: 0,
+  description: Faker::Lorem.paragraph(3, false, 1),
+  image_url: a )
+end
+
+admins_list.each do |a|
+  User.create(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.safe_email,
+  city: Faker::Address.city,
+  country: Faker::Address.country,
+  username: Faker::Internet.user_name,
+  password: Faker::Internet.password,
+  role: 1,
+  description: Faker::Lorem.paragraph(3, false, 1),
   image_url: a )
 end
