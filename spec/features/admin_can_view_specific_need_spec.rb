a6889cd5b93e0387266f5758d5c61b346e34f653
Kris Foss <kris.foss@gmail.com>
2016-05-16 18:31:58 -0600

    tests passing for need type show

--- spec/features/admin_can_view_specific_need_spec.rb
+++ spec/features/admin_can_view_specific_need_spec.rb
@@ -6,20 +6,28 @@ feature "Admin can view a specific need show page" do
     admin = User.create(first_name: "Kris", last_name: "Foss", email: "kris.foss@gmail.com", password: "password", role: 1, username: "Kris", country: "US")
 
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
+    category = Category.create(name: "animals")
 
-    need = NeedType.create(
-    name: "Cow",
-    description: "Bovine Lactation",
-    cost: 100,
-    category_id: 1,
-    image_url:  need_url )
+    need = category.need_types.create(name: "Cow",
+                           description: "Bovine Lactation",
+                           cost: 46,
+                           image_url: "auto_maintenance.png",
+                           slug: "auto-maintenance",
+                           category_id: 1)
+
+    need2 = category.need_types.create(name: "Sheep",
+                           description: "For Wool and Hugs",
+                           cost: 46,
+                           image_url: "sheep.png",
+                           slug: "sheep",
+                           category_id: 1)
 
     visit admin_need_types_path
     click_link "Cow"
-    
-    expect(page).to have_content("Welcome Administrator : #{admin.first_name}")
-    expect(page).to have_content("Role: admin")
-    expect(page).to have_content("Username: #{admin.username}")
-    expect(page).to have_content("Email: #{admin.email}")
+
+    expect(page).to have_content("Cow")
+    expect(page).to have_content("Bovine Lactation")
+    expect(page).to_not have_content("Sheep")
+    expect(page).to_not have_content("For Wool and Hugs")
   end
 end
