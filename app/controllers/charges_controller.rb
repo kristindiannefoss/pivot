6f526c7b75bd21ac652cac288c38784850677129
Jonathan <jonacom@lissismore.com>
2016-05-17 20:26:47 -0600

    still almost done

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,23 +3,28 @@ class ChargesController < ApplicationController
   end
 
   def create
-    # Amount in cents
-    @amount = 500
+    require "pry"; binding.pry
+    begin
+      @amount = @cart.total_donor_cost * 100
 
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => params[:stripeToken]
+      )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Goats and Soda Donation',
+        :currency    => 'usd'
+      )
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    else
+      current_user.donations.find(@cart.contents["donor"].values.flatten).update_all(status: "completed")
+    end
+    require "pry"; binding.pry
   end
 end
