59254dce5011bed8735cc3c32557afa3094585f8
Jonathan Liss <jonacom@lissismore.com>
2016-05-19 12:46:57 -0600

    Minor style changes, fix some tests (#165)

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,7 +1,6 @@
 class Donation < ActiveRecord::Base
   belongs_to :user
   belongs_to :need
-  has_many :donation_amounts
   has_many :needs, through: :donation_amounts
 
   validates_presence_of :status,
